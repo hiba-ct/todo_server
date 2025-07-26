b59fb245c559c7ea9b33d7e563d24ea6c3f38c7a
Semeena Hiba <semeenahiba@gmail.com>
2025-07-26 21:06:31 +0530

    railway connect success

--- index.js
+++ index.js
@@ -12,34 +12,19 @@ app.use(cors());
 
 
 // ✅ MySQL Connection using Railway DB credentials
-  /* const db = mysql.createPool({
+  const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });  
-  */
+ 
 
 
 
 
-const db = mysql.createConnection({
-  host: 'crossover.proxy.rlwy.net',
-  user: 'root',
-  password: 'rgRoyiUfyYUPMWxWuVehoWYVjpivICkG',
-  database: 'railway',
-  port: 49750
-});
 
-db.connect((err) => {
-  if (err) {
-    console.error('❌ Database connection failed:', err.message);
-  } else {
-    console.log('✅ Connected to Railway MySQL database!');
-  }
-  db.end();
-});
 
 // ✅ Connect to DB
   /* const db = mysql.createConnection({
